c322ca10134ee0c4459f6a0f0c2563f6a7217142
Konstantin Chugalinskiy <kchugalinskiy@yandex.ru>
2016-01-14 01:13:34 +0300

    request processor stub

--- Server/RequestProcessor.cpp
+++ Server/RequestProcessor.cpp
@@ -1,9 +1,21 @@
 #include "RequestProcessor.hpp"
 
-#include "Logger/Logger.hpp"
-
 namespace Server
 {
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+RequestProcessor::RequestProcessor()
+    : partialSum(0)
+    , numberOfElements(0)
+{
 
+}
 
+int RequestProcessor::AddNewNumber( int number )
+{
+    std::unique_lock<std::mutex> processorLock(requestProcessorMutex);
+    ++numberOfElements;
+    partialSum += number * number;
+    return (partialSum / numberOfElements);
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 } // namespace Server
--- Server/RequestProcessor.hpp
+++ Server/RequestProcessor.hpp
@@ -1,8 +1,26 @@
 #pragma once
 
+#include <cstdint>
+#include "Logger/Logger.hpp"
+#include <mutex>
+
 namespace Server
 {
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class RequestProcessor
+{
+public:
+    RequestProcessor();
+    // Add new number and return current sum
+    int AddNewNumber( int number );
 
+private:
+    // Disallowed
+    RequestProcessor(RequestProcessor&&);
+    RequestProcessor(const RequestProcessor&);
+    void operator=(const RequestProcessor&);
 
-
+    std::mutex requestProcessorMutex;
+};
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 } // namespace Server
