db09f6a71351dd48d712bf112998075a5a9d9b3e
Konstantin Chugalinskiy <k.chugalinskiy@corp.mail.ru>
2016-01-14 14:49:08 +0300

    Migration to boost command line

--- Client/Main.cpp
+++ Client/Main.cpp
@@ -1,17 +1,27 @@
+#include <boost/program_options.hpp>
 #include "Client.hpp"
 #include "Logger/Logger.hpp"
 
 int main(int argc, const char *argv[])
 {
-    Logger::LoggerConfig logConfig( "D:\\Test\\client.txt" );
+    Logger::LoggerConfig logConfig("D:\\Test\\client.txt");
     LOG_INFO("Starting client...");
 
     try
     {
-        if (argc != 3)
-        {
-            throw std::logic_error( "Usage: blocking_tcp_echo_client <host> <port>" );
-        }
+        std::string host = "localhost";
+        short connectPort = 12345;
+
+        boost::program_options::options_description optionDesc("General options");
+        optionDesc.add_options()
+            ("help,h", "Show help")
+            ("port,p", boost::program_options::value<short>(&connectPort), "Select port")
+            ("host,server,s", boost::program_options::value<std::string>(&host), "Select host");
+
+        boost::program_options::variables_map variablesMap;
+        boost::program_options::parsed_options parsed = boost::program_options::command_line_parser(argc, argv).options(optionDesc).allow_unregistered().run();
+        boost::program_options::store(parsed, variablesMap);
+        boost::program_options::notify(variablesMap);
 
         Client::Client client(argv[1], argv[2]);
         client.Start();
--- Server/Main.cpp
+++ Server/Main.cpp
@@ -1,21 +1,24 @@
 #include "Logger/Logger.hpp"
 #include "Server.hpp"
-#include <boost/lexical_cast.hpp>
 #include <boost/program_options.hpp>
 
 int main(int argc, const char *argv[])
 {
-    short listenPort;
-    std::string serializationPath;
-    int serializationDelay;
+    short listenPort = 12345;
+    std::string serializationPath = "C:\\serverSerializeDefault.txt";
+    int serializationDelay = 1000;
 
     boost::program_options::options_description optionDesc("General options");
     optionDesc.add_options()
         ("help,h", "Show help")
         ("listen_port,l", boost::program_options::value<short>(&listenPort), "Select server listen port")
         ("serialization_path,p", boost::program_options::value<std::string>(&serializationPath), "Select server serialization path")
-        ("serialization_delay,d", boost::program_options::value<int>(&serializationDelay), "Select server serialization delay")
-        ;
+        ("serialization_delay,d", boost::program_options::value<int>(&serializationDelay), "Select server serialization delay");
+
+    boost::program_options::variables_map variablesMap;
+    boost::program_options::parsed_options parsed = boost::program_options::command_line_parser(argc, argv).options(optionDesc).allow_unregistered().run();
+    boost::program_options::store(parsed, variablesMap);
+    boost::program_options::notify(variablesMap);
 
     Logger::LoggerConfig logConfig( "D:\\Test\\server.txt" );
     LOG_INFO("Starting server...");
